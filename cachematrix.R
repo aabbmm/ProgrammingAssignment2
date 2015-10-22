73624beeb6d46b3166ed98f7b3bfe4f9de3bdf3f
aabbmm <aabbmm@gmail.com>
2015-10-22 11:08:12 -0500

    added comments to code

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,13 @@
 makeCacheMatrix <- function(mat=matrix()){
       
+      # This function parallels makeVector in example code for the assignment
+      # The  matrix is stored in the mat variable and is accessed/modified by the 
+      # with the get/set functions.
+      # The inverse is stored in the inv and and is accessed/modified by the
+      # getInverse/setInverse  function.
+      # The function returns the list of get/set and getInverse/setInverse
+      # functions
+      
       inv <- NULL
       set <- function(y_mat){
             mat <<- y_mat
@@ -13,10 +21,13 @@ makeCacheMatrix <- function(mat=matrix()){
 
 cacheSolve <- function(m,...){
       
-      x <- m$getInverse()
-      if (is.null(x)){
+      x <- m$getInverse()         #get inverse variable stored in m, store in x
+      if (is.null(x)){    
+            
+            #if no inverse stored, compute inverse and store in m and x
+            
             x <- solve(m$get(),...)
             m$setInverse(x)
       }
-      x
+      x                    #return inverse as stored in x
 }
